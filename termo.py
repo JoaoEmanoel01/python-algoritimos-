c81770430c349451ed80c427368fcd6be6328889
JoaoEmanoel01 <je7807055@gmail.com>
2024-08-08 17:07:38 -0300

    Create termo.py

---
+++ termo.py
@@ -0,0 +1,7 @@
+t1 = int(input("Digite o primeiro termo: "))
+quan = int(input("Digite a quantidade de termo: "))
+razao = int(input("Digite a razao: "))
+y = 0 
+for i in range(t1, quan, razao):
+    y =+ i 
+print(y)
